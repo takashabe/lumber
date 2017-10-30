4ad8e6aaff3197ba6a0b1bbbff6510faadd23d60
takashabe <takashabe.public@gmail.com>
2017-10-30 20:00:00 +0900

    Add base datastore driver

---
+++ datastore/datastore.go
@@ -0,0 +1,67 @@
+package datastore
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql" // mysql driver
+)
+
+// Datastore represent MySQL adapter
+type Datastore struct {
+	Conn *sql.DB
+}
+
+// NewDatastore returns initialized Datastore
+func NewDatastore() (*Datastore, error) {
+	getEnvWithDefault := func(name, def string) string {
+		if env := os.Getenv(name); len(env) != 0 {
+			return env
+		}
+		return def
+	}
+
+	user := getEnvWithDefault("DB_USER", "root")
+	password := getEnvWithDefault("DB_PASSWORD", "")
+	host := getEnvWithDefault("DB_HOST", "localhost")
+	port := getEnvWithDefault("DB_PORT", "3306")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/lumber?parseTime=true", user, password, host, port)
+
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+	db.SetMaxIdleConns(32)
+	db.SetMaxOpenConns(32)
+
+	return &Datastore{Conn: db}, nil
+}
+
+// Close calls DB.Close
+func (d *Datastore) Close() error {
+	return d.Conn.Close()
+}
+
+func (d *Datastore) query(q string, args ...interface{}) (*sql.Rows, error) {
+	stmt, err := d.Conn.Prepare(q)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	return stmt.Query(args...)
+}
+
+func (d *Datastore) queryRow(q string, args ...interface{}) (*sql.Row, error) {
+	stmt, err := d.Conn.Prepare(q)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	return stmt.QueryRow(args...), nil
+}
