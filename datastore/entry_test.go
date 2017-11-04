6a5aeb87ca6823d092ae47d9ed1ba1893753429a
takashabe <takashabe.public@gmail.com>
2017-11-04 18:12:47 +0900

    Add entry_test

---
+++ datastore/entry_test.go
@@ -0,0 +1,90 @@
+package datastore
+
+import (
+	"bytes"
+	"database/sql"
+	"testing"
+
+	"github.com/takashabe/go-fixture"
+	"github.com/takashabe/lumber/config"
+)
+
+func TestFindEntryByID(t *testing.T) {
+	db, err := NewDatastore()
+	if err != nil {
+		t.Fatalf("want non error, got %v", err)
+	}
+	f := fixture.NewFixture(db.Conn, "mysql")
+	err = f.Load("fixture/entries.yml")
+	if err != nil {
+		t.Fatalf("want non error, got %v", err)
+	}
+
+	cases := []struct {
+		input     int
+		expectID  int
+		expectErr error
+	}{
+		{1, 1, nil},
+		{0, 0, sql.ErrNoRows},
+	}
+	for i, c := range cases {
+		model, err := db.FindEntryByID(c.input)
+		if err != c.expectErr {
+			t.Errorf("#%d: want error %v, got %v", i, c.expectErr, err)
+		}
+
+		if model.ID != c.expectID {
+			t.Errorf("#%d: want id %d, got %d", i, c.expectID, model.ID)
+		}
+	}
+}
+
+func TestSaveEntry(t *testing.T) {
+	db, err := NewDatastore()
+	if err != nil {
+		t.Fatalf("want non error, got %v", err)
+	}
+
+	randomStringWithBytes := func(size int) string {
+		buf := bytes.NewBuffer(make([]byte, 0, size))
+		for i := 0; i < size; i++ {
+			if err := buf.WriteByte('a'); err != nil {
+				t.Fatalf("want non error, got %v", err)
+			}
+		}
+		return buf.String()
+	}
+
+	cases := []struct {
+		inputTitle   string
+		inputContent string
+		inputStatus  int
+		expectErr    error
+	}{
+		{
+			randomStringWithBytes(1 << 8),
+			randomStringWithBytes(1<<16 - 1),
+			0,
+			nil,
+		},
+		{
+			randomStringWithBytes(1 << 8),
+			randomStringWithBytes(1 << 16),
+			0,
+			config.ErrEntrySizeLimitExceeded,
+		},
+		{
+			"",
+			"foo",
+			0,
+			config.ErrEmptyEntry,
+		},
+	}
+	for i, c := range cases {
+		err := db.SaveEntry(c.inputTitle, c.inputContent, c.inputStatus)
+		if err != c.expectErr {
+			t.Errorf("#%d: want error %v, got %v", i, c.expectErr, err)
+		}
+	}
+}
