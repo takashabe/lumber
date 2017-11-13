dada64324cba058c17d91ce68cf2a40d002dd843
takashabe <takashabe.public@gmail.com>
2017-11-14 02:26:24 +0900

    Add server.PostEntry

--- server/entry.go
+++ server/entry.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/takashabe/lumber/model"
@@ -14,4 +15,29 @@ func (s *Server) GetEntry(w http.ResponseWriter, r *http.Request, id int) {
 		return
 	}
 	JSON(w, http.StatusOK, entry)
+}
+
+// PostEntry create new entry
+func (s *Server) PostEntry(w http.ResponseWriter, r *http.Request) {
+	raw := struct {
+		data   []byte
+		status int
+	}{}
+	err := json.NewDecoder(r.Body).Decode(&raw)
+	if err != nil {
+		Error(w, http.StatusNotFound, err, "failed to parsed request")
+		return
+	}
+
+	entry, err := model.NewEntry(raw.data, model.EntryStatus(raw.status))
+	if err != nil {
+		Error(w, http.StatusNotFound, err, "failed to create new entry")
+		return
+	}
+	response := struct {
+		id int
+	}{
+		id: entry.ID,
+	}
+	JSON(w, http.StatusOK, response)
 }
