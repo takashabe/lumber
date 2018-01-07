325253f4daff18f54b89fc6e12827d293c1f490b
takashabe <takashabe.public@gmail.com>
2018-01-07 20:00:00 +0900

    Move util methods to main_test.go

--- interfaces/entry_test.go
+++ interfaces/entry_test.go
@@ -4,37 +4,11 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/takashabe/lumber/helper"
-	"github.com/takashabe/lumber/infrastructure/persistence"
 )
-
-func setupServer(t *testing.T) *httptest.Server {
-	repo, err := persistence.NewEntryRepository()
-	if err != nil {
-		t.Fatalf("want non error, got %#v", err)
-	}
-	server := &Server{
-		Entry: NewEntryHandler(repo),
-	}
-	return httptest.NewServer(server.Routes())
-}
-
-func sendRequest(t *testing.T, method, url string, body io.Reader) *http.Response {
-	req, err := http.NewRequest(method, url, body)
-	if err != nil {
-		t.Fatalf("want non error, got %#v", err)
-	}
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		t.Fatalf("want non error, got %#v", err)
-	}
-	return res
-}
 
 func TestGetEntry(t *testing.T) {
 	ts := setupServer(t)
--- interfaces/main_test.go
+++ interfaces/main_test.go
@@ -1,13 +1,42 @@
 package interfaces
 
 import (
+	"io"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 
 	"github.com/takashabe/lumber/helper"
+	"github.com/takashabe/lumber/infrastructure/persistence"
 )
 
 func TestMain(m *testing.M) {
 	helper.SetupTables()
 	os.Exit(m.Run())
+}
+
+func setupServer(t *testing.T) *httptest.Server {
+	entryRepo, err := persistence.NewEntryRepository()
+	tokenRepo, err := persistence.NewTokenRepository()
+	if err != nil {
+		t.Fatalf("want non error, got %#v", err)
+	}
+	server := &Server{
+		Entry: NewEntryHandler(entryRepo),
+		Token: NewTokenHandler(tokenRepo),
+	}
+	return httptest.NewServer(server.Routes())
+}
+
+func sendRequest(t *testing.T, method, url string, body io.Reader) *http.Response {
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		t.Fatalf("want non error, got %#v", err)
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatalf("want non error, got %#v", err)
+	}
+	return res
 }
