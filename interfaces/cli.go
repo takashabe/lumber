7ce2f9014117aaef0d9a859a7cb7d51d2641fc35
takashabe <takashabe.public@gmail.com>
2018-04-04 21:47:02 +0900

    Use config file at server parameters

--- interfaces/cli.go
+++ interfaces/cli.go
@@ -1,17 +1,11 @@
 package interfaces
 
 import (
-	"flag"
 	"fmt"
 	"io"
 
-	"github.com/pkg/errors"
 	"github.com/takashabe/lumber/infrastructure/persistence"
-)
-
-// default parameters
-const (
-	defaultPort = 80
+	"github.com/takashabe/lumber/library/config"
 )
 
 // Exit codes. used only in Run()
@@ -20,19 +14,8 @@ const (
 
 	// Specific error codes. begin 10-
 	ExitCodeError = 10 + iota
-	ExitCodeParseError
-	ExitCodeInvalidArgsError
 	ExitCodeSetupServerError
 )
-
-var (
-	// ErrParseFailed is failed to cli args parse
-	ErrParseFailed = errors.New("failed to parse args")
-)
-
-type param struct {
-	port int
-}
 
 // CLI is the command line interface object
 type CLI struct {
@@ -42,12 +25,7 @@ type CLI struct {
 
 // Run invokes the CLI with the given arguments
 func (c *CLI) Run(args []string) int {
-	param := &param{}
-	err := c.parseArgs(args[1:], param)
-	if err != nil {
-		fmt.Fprintf(c.ErrStream, "args parse error: %v", err)
-		return ExitCodeParseError
-	}
+	conf := config.Config.Server
 
 	entryRepository, err := persistence.NewEntryRepository()
 	tokenRepository, err := persistence.NewTokenRepository()
@@ -55,6 +33,7 @@ func (c *CLI) Run(args []string) int {
 		fmt.Fprintf(c.ErrStream, "failed to initialized persistence repository: %v", err)
 		return ExitCodeSetupServerError
 	}
+
 	server := Server{
 		Entry: NewEntryHandler(
 			entryRepository,
@@ -65,22 +44,9 @@ func (c *CLI) Run(args []string) int {
 		),
 	}
 
-	if err := server.Run(param.port); err != nil {
+	if err := server.Run(conf.Port); err != nil {
 		fmt.Fprintf(c.ErrStream, "failed from server: %v", err)
 		return ExitCodeError
 	}
 	return ExitCodeOK
-}
-
-func (c *CLI) parseArgs(args []string, p *param) error {
-	flags := flag.NewFlagSet("param", flag.ContinueOnError)
-	flags.SetOutput(c.ErrStream)
-
-	flags.IntVar(&p.port, "port", defaultPort, "Running port. require unused port.")
-
-	err := flags.Parse(args)
-	if err != nil {
-		return errors.Wrapf(ErrParseFailed, err.Error())
-	}
-	return nil
 }
