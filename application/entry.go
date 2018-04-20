b1b727006dd1e3e6940fd96b5fda2bfe229f1905
takashabe <takashabe.public@gmail.com>
2018-04-20 12:35:31 +0900

    Enable "[WIP]" title support

--- application/entry.go
+++ application/entry.go
@@ -70,14 +70,17 @@ func (i *EntryInteractor) Post(e *EntryElement) (int, error) {
 	if !e.Status.IsValid() {
 		return 0, errors.Errorf("invalid entry status type: %d", e.Status)
 	}
-	return i.entryRepo.Save(e.Entity())
+	entry := e.Entity()
+	entry.UpdateStatusByTitle()
+	return i.entryRepo.Save(entry)
 }
 
 // Edit changes entry the title and content
 func (i *EntryInteractor) Edit(id int, e *EntryElement) error {
-	entity := e.Entity()
-	entity.ID = id
-	return i.entryRepo.Edit(entity)
+	entry := e.Entity()
+	entry.ID = id
+	entry.UpdateStatusByTitle()
+	return i.entryRepo.Edit(entry)
 }
 
 // Delete deletes entry
